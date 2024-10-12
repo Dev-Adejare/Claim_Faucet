67073b216363648e6ce57e5c75ddfbfd728f308f
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-10-12 02:33:43 +0100

    update

--- Contract/ClaimFaucet.sol
+++ Contract/ClaimFaucet.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.27;
 import "./DltToken.sol";
 
 contract ClaimFaucet is DltToken{
-    
-    uint256 public constant CLAIMABLE_AMOUNT = 10 * (10**18);
+
+    uint256 public constant CLAIMABLE_AMOUNT = 10;
 
     constructor(string memory _name, string memory _symbol) DltToken(_name, _symbol){}
+
 
     struct User{
         uint256 lastClaimTime;
@@ -17,12 +18,24 @@ contract ClaimFaucet is DltToken{
     mapping (address => User) users;
 
     mapping (address => bool) hasClaimedBefore;
-    
+
+    event TokenClaimSuccessful(address indexed user, uint256 _amount, uint256 _time);
+
 
     function claimToken() public {
-        require(msg.sender != address(0), "Zero address not allowe");
+        require(msg.sender != address(0), "Zero address not allowed");
 
         if(hasClaimedBefore[msg.sender]){
+
+            User storage currentUser = users[msg.sender];
+            require(block.timestamp > (currentUser.lastClaimTime + 1 days), "You claim once after 24 hours");
+
+            currentUser.lastClaimTime = block.timestamp;
+            currentUser.totalClaimed += CLAIMABLE_AMOUNT;
+
+            mint(CLAIMABLE_AMOUNT, msg.sender);
+
+            emit TokenClaimSuccessful(msg.sender, CLAIMABLE_AMOUNT, block.timestamp);
 
         }else{
             hasClaimedBefore[msg.sender] = true;
@@ -31,7 +44,17 @@ contract ClaimFaucet is DltToken{
             User memory currentUser;
             currentUser.lastClaimTime = block.timestamp;
             currentUser.totalClaimed = CLAIMABLE_AMOUNT;
+
+            users[msg.sender] = currentUser;
+
+            emit TokenClaimSuccessful(msg.sender, CLAIMABLE_AMOUNT, block.timestamp);
         }
+    }
+
+    function getUserData() external view returns (uint256, uint256){
+        User memory currentUser = users[msg.sender];
+
+        return (currentUser.lastClaimTime, currentUser.totalClaimed);
     }
 
 
