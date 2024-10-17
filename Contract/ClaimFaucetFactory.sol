e82349525c7ddd96dc26158e90e795b038cb0db0
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-10-17 12:24:30 +0100

    update

---
+++ Contract/ClaimFaucetFactory.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+import {ClaimFaucet} from "./ClaimFaucet.sol";
+
+contract ClaimFaucetFactory{
+    
+}
--- Contract/SaveCelo.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
-
-import {IERC20} from "./Icelo.sol";
-
-contract SaveCelo{
-
-}
