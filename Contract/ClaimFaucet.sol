ac8b93ede85677ba1568a8a842e9e55e72f67a94
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-10-17 13:58:40 +0100

    update

--- Contract/ClaimFaucet.sol
+++ Contract/ClaimFaucet.sol
@@ -22,41 +22,37 @@ contract ClaimFaucet is DltToken{
     event TokenClaimSuccessful(address indexed user, uint256 _amount, uint256 _time);
 
 
-    function claimToken() public {
-        require(msg.sender != address(0), "Zero address not allowed");
+    function claimToken(address _address) public {
+        require(_address != address(0), "Zero address not allowed");
 
-        if(hasClaimedBefore[msg.sender]){
+        if(hasClaimedBefore[_address]){
 
-            User storage currentUser = users[msg.sender];
+            User storage currentUser = users[_address];
             require(block.timestamp > (currentUser.lastClaimTime + 1 days), "You claim once after 24 hours");
 
             currentUser.lastClaimTime = block.timestamp;
             
             currentUser.totalClaimed += CLAIMABLE_AMOUNT;
 
-            mint(CLAIMABLE_AMOUNT, msg.sender);
+            mint(CLAIMABLE_AMOUNT, _address);
 
-            emit TokenClaimSuccessful(msg.sender, CLAIMABLE_AMOUNT, block.timestamp);
+            emit TokenClaimSuccessful(_address, CLAIMABLE_AMOUNT, block.timestamp);
 
         }else{
-            hasClaimedBefore[msg.sender] = true;
-            mint(CLAIMABLE_AMOUNT, msg.sender);
+            hasClaimedBefore[_address] = true;
+            mint(CLAIMABLE_AMOUNT, _address);
 
             User memory currentUser;
             currentUser.lastClaimTime = block.timestamp;
             currentUser.totalClaimed = CLAIMABLE_AMOUNT;
 
-            users[msg.sender] = currentUser;
+            users[_address] = currentUser;
 
-            emit TokenClaimSuccessful(msg.sender, CLAIMABLE_AMOUNT, block.timestamp);
+            emit TokenClaimSuccessful(_address, CLAIMABLE_AMOUNT, block.timestamp);
         }
     }
 
-    function getUserData() external view returns (uint256, uint256){
-        User memory currentUser = users[msg.sender];
-
-        return (currentUser.lastClaimTime, currentUser.totalClaimed);
-    }
+   
 
 
 }
